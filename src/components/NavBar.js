bb57134b47a17e6e7535d5a53868ccf5e90abf41
joserss <josee.88@gmail.com>
2022-03-22 12:36:01 -0700

    Add Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,17 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
 import "bulma/css/bulma.min.css"
+import logo from "../img/cover.png"
+import logo2 from "../img/default-monochrome.svg"
 
 
 const NavBar = () => {
 	return (
-		<nav className="navbar p-2" role="navigation" aria-label="main navigation">
+		<nav className="navbar is-light" role="navigation" aria-label="main navigation">
 			<div className="navbar-brand"> 
-				<h1 className="title">AniRec</h1>
+				<a className="navbar-item" href="">
+					<img src={logo2} height="128" width="112"  />
+				</a>
 			</div>
 		</nav>
 	)
